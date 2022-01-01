916c636504f51761ba6958c987baee8c0f568c7b
audriusn <audrius.narkunas@gmail.com>
2022-01-01 13:47:34 +0200

    a

---
+++ webpage/js/script.js
@@ -0,0 +1,6 @@
+function eraseText() {
+    document.getElementById("notetext").value = "";
+    document.getElementById("name").value = "";
+    document.getElementById("phone").value = "";
+    alert("Ačiū! Jūsų žinutę gavome. Netrukus su Jumis susisieksime.");
+}
